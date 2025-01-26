47e67ade0dbc3e47155bdbe99a936dcdd09130cb
Andrew Bortvin <69335349+andrew-bortvin@users.noreply.github.com>
2025-01-26 11:35:22 -0500

    Update subset_avgMap_to_PAR.sh

--- scripts/chrX/PAR/subset_avgMap_to_PAR.sh
+++ scripts/chrX/PAR/subset_avgMap_to_PAR.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-ml bedtools
-ml htslib
-
 awk -F '\t' -v OFS='\t' -v chrom="chrX" '{ print chrom,$1,$2,$3,$4 }' averagedByBase/chrX_PAR.txt  | sed 's/[[:space:]]*$//' > averagedByBase/chrX_PAR.bed
 
 bedtools intersect -a averagedByBase/chrX_PAR.bed -b chm13v2.0_PAR.bed > averagedByBase/chrX_PAR_isec.bed
