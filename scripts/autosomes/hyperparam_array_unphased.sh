30ea2c1e383d084cabc7d3e042561d93bc89ec55
Andrew Bortvin <69335349+andrew-bortvin@users.noreply.github.com>
2025-01-26 22:31:02 -0500

    Update hyperparam_array_unphased.sh

--- scripts/autosomes/hyperparam_array_unphased.sh
+++ scripts/autosomes/hyperparam_array_unphased.sh
@@ -1,9 +1,9 @@
-pop_info=$(sed "${SLURM_ARRAY_TASK_ID}q;d" make_table_pop_sizes.txt)
+pop_info=$(sed "${SLURM_ARRAY_TASK_ID}q;d" ../../inputsmake_table_pop_sizes.txt)
 pop=`cut -f1 -d' '<<<${pop_info}`
 n=`cut -f2 -d' '<<<${pop_info}`
 
 
 pyrho hyperparam --samplesize ${n} --tablefile ~/scr16_rmccoy22/abortvi2/pyrho_1kgp_chm13_array/lookup_tables/${pop}_lookuptable.hdf \
 --mu 1.25e-8 --logfile logs/hyperparam/${pop}_hyperparam.log --ploidy 2 \
---smcpp_file ~/scr16_rmccoy22/abortvi2/pyrho_1kgp_chm13_array/smcpp_popsizes_1kg/${pop}_pop_sizes.csv --outfile hyperparam_results/${pop}_hyperparam_results.txt \
+--smcpp_file ../../inputs/smcpp_popsizes_1kg/${pop}_pop_sizes.csv --outfile hyperparam_results/${pop}_hyperparam_results.txt \
 --numthreads 48
