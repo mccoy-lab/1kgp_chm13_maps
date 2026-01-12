d447a735c64a48ea26f8dc1b5d161bc7e6e81154
Andrew Bortvin <69335349+andrew-bortvin@users.noreply.github.com>
2026-01-11 20:58:46 -0500

    Update subset_pops_unrelated.sh

--- scripts/preprocessing/subset_pops_unrelated.sh
+++ scripts/preprocessing/subset_pops_unrelated.sh
@@ -8,4 +8,4 @@ fi
 pop_info=$(sed "${pop_number}q;d" make_table_pop_sizes.txt)
 pop=`cut -f1 -d' '<<<${pop_info}`
 
-bcftools view /home/abortvi2/scr16_rmccoy22/abortvi2/vcf_shapeit5/phased_T2T_panel/1KGP.CHM13v2.0.chr${chr}.recalibrated.snp_indel.pass.phased.vcf.gz -S pop_individuals/${pop}.txt -o unrelated/${pop}_chr${chr}.vcf.gz --threads 24
+bcftools view vcf_shapeit5/phased_T2T_panel/1KGP.CHM13v2.0.chr${chr}.recalibrated.snp_indel.pass.phased.vcf.gz -S pop_individuals/${pop}.txt -o unrelated/${pop}_chr${chr}.vcf.gz --threads 24
