a6b85fa7585770660b620f9ddd3366a9d85e1ae5
Andrew Bortvin <69335349+andrew-bortvin@users.noreply.github.com>
2026-01-11 20:59:15 -0500

    Update subset_chrX.sh

--- scripts/chrX/subset_chrX.sh
+++ scripts/chrX/subset_chrX.sh
@@ -3,4 +3,4 @@ pop_number=${SLURM_ARRAY_TASK_ID}
 pop_info=$(sed "${pop_number}q;d" make_table_pop_sizes.txt)
 pop=`cut -f1 -d' '<<<${pop_info}`
 
-~/scr4_rmccoy22/abortvi2/bcftools-1.14/bcftools view /home/abortvi2/scr16_rmccoy22/abortvi2/vcf_shapeit5/phased_T2T_panel/1KGP.CHM13v2.0.chrX.recalibrated.snp_indel.pass.phased.vcf.gz -S pop_individuals/${pop}.txt -o chrX_vcfs/${pop}_chrX.vcf.gz --threads 24
+bcftools view vcf_shapeit5/phased_T2T_panel/1KGP.CHM13v2.0.chrX.recalibrated.snp_indel.pass.phased.vcf.gz -S pop_individuals/${pop}.txt -o chrX_vcfs/${pop}_chrX.vcf.gz --threads 24
