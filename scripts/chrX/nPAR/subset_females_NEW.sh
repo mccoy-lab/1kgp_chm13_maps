5295da3c0132dab4e3590650e277cbf215e4c613
Andrew Bortvin <69335349+andrew-bortvin@users.noreply.github.com>
2026-01-11 21:11:57 -0500

    Update subset_females_NEW.sh

--- scripts/chrX/nPAR/subset_females_NEW.sh
+++ scripts/chrX/nPAR/subset_females_NEW.sh
@@ -3,4 +3,4 @@ pop_number=${SLURM_ARRAY_TASK_ID}
 pop_info=$(sed "${pop_number}q;d" make_table_pop_sizes.txt)
 pop=`cut -f1 -d' '<<<${pop_info}`
 
-~/scr4_rmccoy22/abortvi2/bcftools-1.14/bcftools view chrX_New_vcfs_masked/${pop}_chrX_masked.vcf.gz -S femaleIndividualNames/${pop}_females.txt -o chrX_female_vcfs_NEW/${pop}_chrX_female.vcf.gz --threads 24
+bcftools view chrX_New_vcfs_masked/${pop}_chrX_masked.vcf.gz -S femaleIndividualNames/${pop}_females.txt -o chrX_female_vcfs_NEW/${pop}_chrX_female.vcf.gz --threads 24
