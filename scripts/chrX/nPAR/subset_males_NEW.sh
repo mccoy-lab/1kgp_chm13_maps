3690d48eb02b761f0940965bb1ce5c580f54e7bf
Andrew Bortvin <69335349+andrew-bortvin@users.noreply.github.com>
2026-01-11 21:11:45 -0500

    Update subset_males_NEW.sh

--- scripts/chrX/nPAR/subset_males_NEW.sh
+++ scripts/chrX/nPAR/subset_males_NEW.sh
@@ -3,4 +3,4 @@ pop_number=${SLURM_ARRAY_TASK_ID}
 pop_info=$(sed "${pop_number}q;d" make_table_pop_sizes.txt)
 pop=`cut -f1 -d' '<<<${pop_info}`
 
-~/scr4_rmccoy22/abortvi2/bcftools-1.14/bcftools view chrX_New_vcfs_masked/${pop}_chrX_masked.vcf.gz -S maleIndividualNames/${pop}_males.txt -o chrX_male_vcfs_NEW/${pop}_chrX_male.vcf.gz --threads 24
+bcftools view chrX_New_vcfs_masked/${pop}_chrX_masked.vcf.gz -S maleIndividualNames/${pop}_males.txt -o chrX_male_vcfs_NEW/${pop}_chrX_male.vcf.gz --threads 24
