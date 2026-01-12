60ca1f05ba71e1c77e43c1f493d7d6bc560092b8
Andrew Bortvin <69335349+andrew-bortvin@users.noreply.github.com>
2026-01-11 21:07:46 -0500

    Update pyrho_optimize_unphased_PAR.sh

--- scripts/chrX/PAR/pyrho_optimize_unphased_PAR.sh
+++ scripts/chrX/PAR/pyrho_optimize_unphased_PAR.sh
@@ -2,7 +2,7 @@ pop_number=${SLURM_ARRAY_TASK_ID}
 pop_info=$(sed "${pop_number}q;d" make_table_pop_sizes.txt)
 pop=`cut -f1 -d' '<<<${pop_info}`
 
-window_size=`grep -h L2 ~/scr16_rmccoy22/abortvi2/pyrho_1kgp_chm13_array/optimize_paramaters/${pop}_optimize_params.txt | cut -f2 -d' '`
-block_penalty=`grep -h L2 ~/scr16_rmccoy22/abortvi2/pyrho_1kgp_chm13_array/optimize_paramaters/${pop}_optimize_params.txt | cut -f1 -d' '`
+window_size=`grep -h L2 pyrho_1kgp_chm13_array/optimize_paramaters/${pop}_optimize_params.txt | cut -f2 -d' '`
+block_penalty=`grep -h L2 pyrho_1kgp_chm13_array/optimize_paramaters/${pop}_optimize_params.txt | cut -f1 -d' '`
 
-pyrho optimize --tablefile ~/scr16_rmccoy22/abortvi2/pyrho_1kgp_chm13_array/lookup_tables/${pop}_lookuptable.hdf --windowsize ${window_size} --blockpenalty ${block_penalty} --vcffile chrX_PAR/${pop}_chrX_PAR_masked.vcf.gz --outfile opt_params_masked/${pop}_chrX_PAR_w${window_size}_b${block_penalty}_masked.rmap --ploidy 2 --numthreads 48 --logfile logs/opt_params_masked/${pop}_chrX_PAR_w${window_size}_b${block_penalty}_masked.log
+pyrho optimize --tablefile pyrho_1kgp_chm13_array/lookup_tables/${pop}_lookuptable.hdf --windowsize ${window_size} --blockpenalty ${block_penalty} --vcffile chrX_PAR/${pop}_chrX_PAR_masked.vcf.gz --outfile opt_params_masked/${pop}_chrX_PAR_w${window_size}_b${block_penalty}_masked.rmap --ploidy 2 --numthreads 48 --logfile logs/opt_params_masked/${pop}_chrX_PAR_w${window_size}_b${block_penalty}_masked.log
