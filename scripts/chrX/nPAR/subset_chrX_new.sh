6967f41906933b62d64065ec352a3aef549dad9f
Andrew Bortvin <69335349+andrew-bortvin@users.noreply.github.com>
2026-01-11 21:12:10 -0500

    Update subset_chrX_new.sh

--- scripts/chrX/nPAR/subset_chrX_new.sh
+++ scripts/chrX/nPAR/subset_chrX_new.sh
@@ -3,4 +3,4 @@ pop_number=${SLURM_ARRAY_TASK_ID}
 pop_info=$(sed "${pop_number}q;d" make_table_pop_sizes.txt)
 pop=`cut -f1 -d' '<<<${pop_info}`
 
-~/scr4_rmccoy22/abortvi2/bcftools-1.14/bcftools view chrX_filtered_HR.vcf.gz -S pop_individuals/${pop}.txt -o chrX_New_byPop_vcfs/${pop}_chrX.vcf.gz --threads 24
+bcftools view chrX_filtered_HR.vcf.gz -S pop_individuals/${pop}.txt -o chrX_New_byPop_vcfs/${pop}_chrX.vcf.gz --threads 24
