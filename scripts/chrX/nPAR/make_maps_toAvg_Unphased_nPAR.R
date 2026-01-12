1a7ce659ead76bcff3ab473b746051640c21abbc
Andrew Bortvin <69335349+andrew-bortvin@users.noreply.github.com>
2026-01-11 21:16:31 -0500

    Update make_maps_toAvg_Unphased_nPAR.R

--- scripts/chrX/nPAR/make_maps_toAvg_Unphased_nPAR.R
+++ scripts/chrX/nPAR/make_maps_toAvg_Unphased_nPAR.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 
-popFiles <- list.files('/home/abortvi2/scr16_rmccoy22/abortvi2/pyrho_1kgp_chm13_unphased/chrX_nPAR/opt_params_masked_masked')
+popFiles <- list.files('pyrho_1kgp_chm13_unphased/chrX_nPAR/opt_params_masked_masked')
 
 for (f in popFiles){
   pop <-  strsplit(f, '_')[[1]][1]
   
   # Get the list of optimized parameters for this population
-  optimized_params <- read.table(paste0('/home/abortvi2/scr16_rmccoy22/abortvi2/pyrho_1kgp_chm13_unphased/chrX_nPAR/optimize_paramaters/',
+  optimized_params <- read.table(paste0('pyrho_1kgp_chm13_unphased/chrX_nPAR/optimize_paramaters/',
                                         pop, '_optimize_params_nPAR.txt'),
                                  header = FALSE,
                                  col.names = c('Block_Penalty', 'Window_Size', 'Optimized_Parameter'))
@@ -14,7 +14,7 @@ for (f in popFiles){
   window <- optimized_params$Window_Size[grepl("L2", optimized_params$Optimized_Parameter)]
   penalty <- optimized_params$Block_Penalty[grepl("L2", optimized_params$Optimized_Parameter)]
   
-  in_name <- paste0('/home/abortvi2/scr16_rmccoy22/abortvi2/pyrho_1kgp_chm13_unphased/chrX_nPAR/opt_params_masked_masked/', f)
+  in_name <- paste0('pyrho_1kgp_chm13_unphased/chrX_nPAR/opt_params_masked_masked/', f)
 
   map_masked <- read.table(in_name,
                            header=FALSE,
@@ -28,7 +28,7 @@ for (f in popFiles){
   map_masked <- map_masked[map_masked$cM<1,]
 
 
-  out_name <- paste0('/home/abortvi2/scr16_rmccoy22/abortvi2/pyrho_1kgp_chm13_unphased/chrX_nPAR/opt_params_toAvg/', pop, '_chrX_nPAR.tsv')
+  out_name <- paste0('pyrho_1kgp_chm13_unphased/chrX_nPAR/opt_params_toAvg/', pop, '_chrX_nPAR.tsv')
 
 
   write.table(map_masked,
